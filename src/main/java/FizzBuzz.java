6bd14fd48f34ea7b93a92037a4c7e295905f76e5
aiyandaodi <529158785@qq.com>
2019-12-28 12:59:58 +0800

    Refactoring

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,12 +1,12 @@
 import static java.lang.String.format;
 
 public class FizzBuzz {
-    //主函数
+    /*//主函数
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.println(of(i));
         }
-    }
+    }*/
     public static String of(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
