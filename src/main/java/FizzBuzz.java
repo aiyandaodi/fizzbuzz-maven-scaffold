b266c2a5f7b6825019b27357b5c8b8490476a308
aiyandaodi <529158785@qq.com>
2019-12-28 13:14:32 +0800

    Refactoring

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,16 +1,11 @@
 import static java.lang.String.format;
 
 public class FizzBuzz {
-    /*//主函数
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(of(i));
-        }
-    }*/
+
     public static String of(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        }
+//        if (number % 3 == 0 && number % 5 == 0) {
+//            return "FizzBuzz";
+//        }
         if (number % 3 == 0) {
             return "Fizz";
         }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,15 +1,17 @@
+import org.junit.Rule;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
-
 import static org.junit.Assert.assertEquals;
 
 
 @RunWith(Parameterized.class)
 public class FizzBuzzTest {
+
     private int number;
     private String expected;
 
@@ -17,12 +19,13 @@ public class FizzBuzzTest {
         this.number = number;
         this.expected = expected;
     }
+
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
                 {3, "Fizz"},
                 {5, "Buzz"},
-                {15, "FizzBuzz"}
+                //{15, "FizzBuzz"}
         });
     }
 
