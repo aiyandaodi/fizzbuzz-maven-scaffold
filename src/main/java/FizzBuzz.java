3dd9f9261a21c77e0918bd0e9ac0b8e52429791c
aiyandaodi <529158785@qq.com>
2019-12-28 11:29:01 +0800

    Refactoring

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,8 +1,21 @@
+import static java.lang.String.format;
+
 public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100 ; i++) {
+            System.out.println(of(i));
+        }
+    }
     public static String of(int number) {
-        if (number == 3) {
+        if(number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
             return "Fizz";
         }
-        return null;
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return format("%d", number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,15 +1,33 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(Parameterized.class)
 public class FizzBuzzTest {
+    private int number;
+    private String expected;
+
+    public FizzBuzzTest(int number, String expected) {
+        this.number = number;
+        this.expected = expected;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {3, "Fizz"},
+                {5, "Buzz"},
+                {15, "FizzBuzz"}
+        });
+    }
+
     @Test
-    public void shouldReturnFizzGivenNumberDivisibleBy3() {
-        //given
-        int number = 3;
-        //when
-        String result = FizzBuzz.of(number);
-        ///then
-        assertThat(result).isEqualTo("Fizz");
+    public void test() {
+        assertEquals(expected, FizzBuzz.of(number));
     }
 }
